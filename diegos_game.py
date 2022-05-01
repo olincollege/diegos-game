394903bb43d45ad35b3da6d68e5409860ce1914b
Milo Song-Weiss <msongweiss@olin.edu>
2022-05-01 01:45:21 -0400

    Update diegos_game.py

--- diegos_game.py
+++ diegos_game.py
@@ -9,14 +9,23 @@ screen = pygame.display.set_mode(size)
 
 diego = pygame.image.load("diego.png")
 diegorect = diego.get_rect()
+diegospeed = [2, 0]
 
 def main():
+    global diegorect
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()                
                 sys.exit()
+
+        diegorect = diegorect.move(diegospeed)
+
+        if diegorect.left < 0 or diegorect.right > width:
+            diegospeed[0] = -diegospeed[0]
+
         screen.fill(black)
         screen.blit(diego, diegorect)
         pygame.display.flip()
+
 main()
